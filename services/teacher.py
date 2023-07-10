03cbb880a2ede78584750d35dd688107e8c4be67
Luis Armenta <luigiangelxd@gmail.com>
2023-07-10 15:32:52 -0600

    check an unassigned teacher first

--- services/teacher.py
+++ services/teacher.py
@@ -2,15 +2,16 @@ from repositories.teachers_repository import TeacherRepository
 from typing import Optional, List
 from models.teacher import Teacher
 from services.scraper import WebScraper
+import functools
 
 class TeacherService:
   def __init__(self, repository: TeacherRepository, web_scraper: WebScraper):
     self.repository = repository
     self.webscraper = web_scraper
   
+  @functools.lru_cache(maxsize=None)
   def get_teacher(self, name: str) -> Optional[Teacher]:
     name = name.strip().upper()
-    teacher = self.repository.get_teacher(name)
     
     if name == 'SIN ASIGNAR':
       return Teacher(
@@ -21,6 +22,8 @@ class TeacherService:
         polarity=0.5,
         url='https://foroupiicsa.net/diccionario/'
       )
+    
+    teacher = self.repository.get_teacher(name)
     
     if teacher:
       return teacher
