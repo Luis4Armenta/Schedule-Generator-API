fd0d558ffbf91e236854841fc82a2aae9d503879
Luis Armenta <luigiangelxd@gmail.com>
2023-07-13 13:27:09 -0600

    Scrapy deleted

--- services/scrapy/items.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from scrapy import Item, Field
-
-class TeacherItem(Item):
-  name: str = Field()
-  url: str = Field()
-  subjects: List[str] = Field()
-  comments: List = Field()
-  polarity: float = Field()
