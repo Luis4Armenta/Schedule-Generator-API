28c7112e9a6fe632ebdfc16164693ea4f9cc2b43
Luis Armenta <luigiangelxd@gmail.com>
2023-07-14 11:11:21 -0600

    Advanced course search

---
+++ schemas/schedule.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class ScheduleGeneratorRequest(BaseModel):
+  levels: List[str]
+  semesters: List[str]
+  start_time: Optional[str] = '07:00'
+  end_time: Optional[str] = '22:00'
+  career: str
+  unwanted_teachers: List[str] = []
+  shifts: List[str] = ['M', 'V']
+  length: int = 7
+  
