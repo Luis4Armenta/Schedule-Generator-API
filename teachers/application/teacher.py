159a4c76e14564bce843a419cdb66257d98fd822
Luis Armenta <luigiangelxd@gmail.com>
2023-12-10 18:05:24 -0600

    Refactor: Teacher services find and add

--- teachers/application/teacher.py
+++ teachers/application/teacher.py
@@ -44,35 +44,45 @@ class TeacherService:
       else:
         # else teacher was not found in the teacher repo
         
-        # Search comments about him
-        comments: List[Comment] = self.comment_service.seach_comments(teacher_name)
+        teacher: Optional[Teacher] = self.find_teacher(teacher_name)
         
-        # if there are comments
-        if len(comments) != 0:
-          # build teacher entity
-          subjects: List[str] = [c.subject for c in comments]
-          positive_scores: List[float] = [c.positive_score for c in comments]
-          positive_score = get_positive_score(positive_scores)
-          teacher_url = get_url_for_teacher(teacher_name)
-          teacher: Teacher = Teacher(
-            name=teacher_name,
-            comments=comments,
-            subjects=subjects,
-            url=teacher_url,
-            positive_score=positive_score
-          )
-          
-          # save the teacher
-          self.teacher_repository.add_teacher(teacher)
-          
-          return self.teacher_repository.get_teacher(teacher_name)
+        if teacher:
+          return teacher
         else:
-          teacher_url = get_url_for_teacher(teacher_name)
-          return Teacher(
-            name=teacher_name,
-            subjects=[],
-            comments=[],
-            positive_score=0.5,
-            url=teacher_url
-          )
+          return None
           
+  def find_teacher(self, teacher_name: str) -> Optional[Teacher]:
+    # Search comments about him
+    comments: List[Comment] = self.comment_service.seach_comments(teacher_name)
+    
+    # if there are comments
+    if len(comments) != 0:
+      # build teacher entity
+      subjects: List[str] = [c.subject for c in comments]
+      positive_scores: List[float] = [c.positive_score for c in comments]
+      positive_score = get_positive_score(positive_scores)
+      teacher_url = get_url_for_teacher(teacher_name)
+
+      teacher: Teacher = Teacher(
+        name=teacher_name,
+        comments=comments,
+        subjects=subjects,
+        url=teacher_url,
+        positive_score=positive_score
+      )
+      
+      # save the teacher
+      self.add_teacher(teacher)
+      return self.teacher_repository.get_teacher(teacher_name)
+    else:
+      teacher_url = get_url_for_teacher(teacher_name)
+      return Teacher(
+        name=teacher_name,
+        subjects=[],
+        comments=[],
+        positive_score=0.5,
+        url=teacher_url
+      )
+      
+  def add_teacher(self, new_teacher: Teacher) -> None:
+    self.teacher_repository.add_teacher(new_teacher)
